dfb8646701aca38a231bf93e8da3c175dd140a11
nsgupta <nsgupta@isystango.com>
2018-07-27 17:25:23 +0530

    webpack setup

---
+++ src/containers/Users.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Users extends Component{
+    render(){
+        return (
+            <div>
+            <h1>The Pizza</h1>
+            <p>O!!</p>
+            </div>
+        );
+    }
+}
+export default Users;
